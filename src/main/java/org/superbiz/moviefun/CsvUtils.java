81beb6b56c33c0f34dc75c0dd70c465c5ee91f00
Sophie Eichhorn <>
2017-12-06 15:36:37 -0700

    correct path

--- src/main/java/org/superbiz/moviefun/CsvUtils.java
+++ src/main/java/org/superbiz/moviefun/CsvUtils.java
@@ -2,6 +2,7 @@ package org.superbiz.moviefun;
 
 import com.fasterxml.jackson.databind.MappingIterator;
 import com.fasterxml.jackson.databind.ObjectReader;
+import org.superbiz.moviefun.movies.Movie;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -13,8 +14,9 @@ import java.util.Scanner;
 public class CsvUtils {
 
     public static String readFile(String path) {
+
         try {
-            Scanner scanner = new Scanner(new File(path)).useDelimiter("\\A");
+            Scanner scanner = new Scanner(CsvUtils.class.getClassLoader().getResourceAsStream(path)).useDelimiter("\\A");
 
             if (scanner.hasNext()) {
                 return scanner.next();
@@ -22,7 +24,7 @@ public class CsvUtils {
                 return "";
             }
 
-        } catch (FileNotFoundException e) {
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }

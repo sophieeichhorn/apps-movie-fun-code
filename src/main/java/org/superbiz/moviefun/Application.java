fc71037d75801b91d0a5914d79f8e5f9d8057b5f
Sophie Eichhorn <>
2017-12-07 09:04:51 -0700

    ready for assignment

--- src/main/java/org/superbiz/moviefun/Application.java
+++ src/main/java/org/superbiz/moviefun/Application.java
@@ -1,5 +1,9 @@
 package org.superbiz.moviefun;
 
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3Client;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
@@ -16,4 +20,20 @@ public class Application {
     public ServletRegistrationBean actionServletRegistration(ActionServlet actionServlet) {
         return new ServletRegistrationBean(actionServlet, "/moviefun/*");
     }
+
+    @Value("${s3.endpointUrl}") String s3EndpointUrl;
+    @Value("${s3.accessKey}") String s3AccessKey;
+    @Value("${s3.secretKey}") String s3SecretKey;
+    @Value("${s3.bucketName}") String s3BucketName;
+
+    @Bean
+    public BlobStore blobStore() {
+        AWSCredentials credentials = new BasicAWSCredentials(s3AccessKey, s3SecretKey);
+        AmazonS3Client s3Client = new AmazonS3Client(credentials);
+
+        s3Client.setEndpoint(s3EndpointUrl);
+
+        return new S3Store(s3Client, s3BucketName);
+    }
+
 }
--- src/main/java/org/superbiz/moviefun/FileStore.java
+++ src/main/java/org/superbiz/moviefun/FileStore.java
@@ -1,4 +1,46 @@
 package org.superbiz.moviefun;
 
-public class FileStore {
+import org.apache.tika.io.IOUtils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URLConnection;
+import java.util.Optional;
+
+public class FileStore implements BlobStore {
+
+    @Override
+    public void put(Blob blob) throws IOException {
+        File targetFile = new File(blob.name);
+
+        targetFile.delete();
+        targetFile.getParentFile().mkdirs();
+        targetFile.createNewFile();
+
+
+        try (FileOutputStream outputStream = new FileOutputStream(targetFile)) {
+            IOUtils.copy(blob.inputStream, outputStream);
+        }
+
+    }
+
+    @Override
+    public Optional<Blob> get(String name) throws IOException {
+        File file = new File(name);
+
+        if (!file.exists()) {
+            return Optional.empty();
+        }
+
+        return Optional.of(
+                new Blob(name, new FileInputStream(file), URLConnection.guessContentTypeFromName(file.getName()))
+        );
+    }
+
+    @Override
+    public void deleteAll() {
+
+    }
 }
